a615bebf1d70cefa2aad13d8e5a39bc83a3cc7ff
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-03-21 17:49:16 +0530

    Fix for the issue item #5

--- utils/PyramidTree.sh
+++ utils/PyramidTree.sh
@@ -4,28 +4,20 @@
 printf "Enter a number 			: "
 read num
 
-printf "Re Enter the number 	: "
-read newNum
-
-if [ $num = $newNum ] 
+if [ $input -eq $input 2>/dev/null ]
 then
-	if [ $input -eq $input 2>/dev/null ]
-	then
-	    printf "\nPrinting the stars for $num rows \n\n"
-		for (( i=1; i<=$num; i++ )); do
-		    rowSpace=""
-		    star="*"
-		    for (( j=$num; j>i; j--)); do
-		        rowSpace=$rowSpace" "
-		    done
-		    for(( k=1; k<i; k++ )); do
-		        star="$star *"
-		    done
-			echo "$rowSpace$star"
+	printf "\nPrinting the stars for $num rows \n\n"
+	for (( i=1; i<=$num; i++ )); do
+		rowSpace=""
+		star="*"
+		for (( j=$num; j>i; j--)); do
+			rowSpace=$rowSpace" "
 		done
-	else
-		printf "\n$num is not a valid number"
-	fi
+		for(( k=1; k<i; k++ )); do
+			star="$star *"
+		done
+		echo "$rowSpace$star"
+	done
 else
-	printf "\n$Input values are not same. Please enter the same value."
+	printf "\n$num is not a valid number"
 fi
